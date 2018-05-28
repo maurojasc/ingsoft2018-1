f62f9c1b6839ab4fab6bfc853be1420d0077ec35
maurojasc <33296864+maurojasc@users.noreply.github.com>
2018-05-28 07:45:57 -0500

    primera version ejemplogithub

---
+++ src/ejemplogithub/Ejemplogithub.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplogithub;
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Ejemplogithub {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("ejemplo github");// TODO code application logic here
+    }
+    
+}
